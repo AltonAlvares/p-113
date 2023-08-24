976f22e0601ec858232eeb53e07405160577957d
AltonAlvares <112641343+AltonAlvares@users.noreply.github.com>
2023-08-24 17:35:45 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup()
+{
+    canvas=createCanvas(1000, 1000);
+    canvas.position(118, 250);
+}
+function draw()
+{
+    circle(70, 50, 80)
+    ellipse(150, 50, 55, 55)
+    rect(200, 20, 50, 55);
+}
